e2506fc8b4594ada16813f29b547135bba02738c
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-10-20 22:58:48 +0100

    feat: add rota raíz

--- src/routes.ts
+++ src/routes.ts
@@ -1,1 +1,10 @@
-import {} from 'express'
+import {Router, Request, Response} from 'express'
+
+const routes = Router()
+
+routes.get('/', (request: Request, response: Response) =>{
+
+    return response.json({message: 'Hello, World'})
+})
+
+export default routes
