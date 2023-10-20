d5f01eeb035bd2d5c3e0a62647ccbb228433cc6c
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-10-20 23:12:41 +0100

    feat: add server

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import bodyParser = require('body-parser')
+import routes from './routes'
+import * as express from 'express'
 
-AppDataSource.initialize().then(async () => {
+const app = express()
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+app.use(bodyParser.json())
+app.use(routes)
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
-}).catch(error => console.log(error))
+app.listen(3333)
